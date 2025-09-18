a8fd0798ad5dbe51f307e304b79393f2f9d8790e
Windy Phung <windyphung@posteo.de>
2025-09-18 16:24:47 +0200

    Printing strings for patterns

--- src/search/cost_saturation/pho_heuristic.cc
+++ src/search/cost_saturation/pho_heuristic.cc
@@ -12,6 +12,8 @@
 #include "../task_utils/task_properties.h"
 #include "../utils/logging.h"
 #include "projection.h"
+#include "../task_proxy.h"
+#include "types.h"
 
 using namespace std;
 
@@ -24,11 +26,13 @@ namespace cost_saturation {
 */
 PhO::PhO(
     const Abstractions &abstractions, const vector<int> &costs,
-    lp::LPSolverType solver_type, bool saturated, const utils::LogProxy &log)
-    : lp_solver(solver_type), saturated(saturated), log(log) {
-    double infinity = lp_solver.get_infinity();
-    int num_abstractions = abstractions.size();
-    int num_operators = costs.size();
+    lp::LPSolverType solver_type, bool saturated, const utils::LogProxy &log,
+    std::shared_ptr<AbstractTask> task_ptr, bool ppc)
+    : lp_solver(solver_type), solver_type(solver_type), saturated(saturated), log(log), task_ptr(task_ptr), ppc(ppc) {
+        double infinity = lp_solver.get_infinity();
+        int num_abstractions = abstractions.size();
+        int num_operators = costs.size();
+            // ppc is now a member variable
 
     saturated_costs_by_abstraction.reserve(num_abstractions);
     h_values_by_abstraction.reserve(num_abstractions);
@@ -83,7 +87,11 @@ CostPartitioningHeuristic PhO::compute_cost_partitioning(
     const Abstractions &abstractions, const vector<int> &,
     const vector<int> &costs, const vector<int> &abstract_state_ids) {
     int num_abstractions = abstractions.size();
+    cout << "Computing cost partitioning" << endl;
     int num_operators = costs.size();
+
+    // Create a TaskProxy from the stored task_ptr
+    TaskProxy task_proxy(*task_ptr);
 
     double min_h = std::numeric_limits<double>::infinity();
     for (int i = 0; i < num_abstractions; ++i) {
@@ -134,94 +142,102 @@ CostPartitioningHeuristic PhO::compute_cost_partitioning(
             << cp_heuristic.compute_heuristic(abstract_state_ids) << endl;
     }
 
-    // After solving the first LP
-    double prev_obj_value = lp_solver.get_objective_value();
 
-    // Prepare variables for the new LP
-    named_vector::NamedVector<lp::LPVariable> variables;
-    double infinity = lp_solver.get_infinity();
-    // Compute M for the big-M constraint: if min_h > 0, use prev_obj_value / min_h, else use prev_obj_value.
-    double M = (min_h > 0) ? (prev_obj_value / min_h) : prev_obj_value;
+    if (ppc) {
+        // Print for every evaluated state (no guard)
 
-    // Add weight variables (as before)
-    for (int i = 0; i < num_abstractions; ++i) {
-        variables.emplace_back(0, infinity, 0); // objective coeff set below
-    }
+        // Use a separate LP solver for the second LP (printing only)
+        lp::LPSolver print_lp_solver(solver_type);
 
-    // Add binary variables
-    for (int i = 0; i < num_abstractions; ++i) {
-        variables.emplace_back(0, 1, 0, true); // is_integer = true
-    }
+        // After solving the first LP
+        double prev_obj_value = lp_solver.get_objective_value();
 
-    // Prepare constraints (copy from first LP)
-    named_vector::NamedVector<lp::LPConstraint> constraints;
-    constraints.reserve(num_operators);
-    for (int op_id = 0; op_id < num_operators; ++op_id) {
-        lp::LPConstraint constraint(-infinity, costs[op_id]);
+        // Prepare variables for the new LP
+        named_vector::NamedVector<lp::LPVariable> variables;
+        double infinity = lp_solver.get_infinity();
+        // Compute M for the big-M constraint: if min_h > 0, use prev_obj_value / min_h, else use prev_obj_value.
+        double M = (min_h > 0) ? (prev_obj_value / min_h) : prev_obj_value;
+
+        // Add weight variables (as before)
         for (int i = 0; i < num_abstractions; ++i) {
-            if (saturated) {
-                int scf_h = saturated_costs_by_abstraction[i][op_id];
-                if (scf_h == -INF) {
-                    // The constraint is always satisfied and we can ignore it.
-                    continue;
+            variables.emplace_back(0, infinity, 0); // objective coeff set below
+        }
+
+        // Add binary variables
+        for (int i = 0; i < num_abstractions; ++i) {
+            variables.emplace_back(0, 1, 0, true); // is_integer = true
+        }
+
+        // Prepare constraints (copy from first LP)
+        named_vector::NamedVector<lp::LPConstraint> constraints;
+        constraints.reserve(num_operators);
+        for (int op_id = 0; op_id < num_operators; ++op_id) {
+            lp::LPConstraint constraint(-infinity, costs[op_id]);
+            for (int i = 0; i < num_abstractions; ++i) {
+                if (saturated) {
+                    int scf_h = saturated_costs_by_abstraction[i][op_id];
+                    if (scf_h == -INF) {
+                        // The constraint is always satisfied and we can ignore it.
+                        continue;
+                    }
+                    if (scf_h != 0) {
+                        constraint.insert(i, scf_h);
+                    }
+                } else if (
+                    abstractions[i]->operator_is_active(op_id) &&
+                    costs[op_id] != 0) {
+                    constraint.insert(i, costs[op_id]);
                 }
-                if (scf_h != 0) {
-                    constraint.insert(i, scf_h);
-                }
-            } else if (
-                abstractions[i]->operator_is_active(op_id) &&
-                costs[op_id] != 0) {
-                constraint.insert(i, costs[op_id]);
+            }
+            if (!constraint.empty()) {
+                constraints.push_back(move(constraint));
             }
         }
-        if (!constraint.empty()) {
-            constraints.push_back(move(constraint));
+
+        // Add constraint: sum_i w_i * h_i >= prev_obj_value
+        lp::LPConstraint obj_constraint(prev_obj_value, infinity);
+        for (int i = 0; i < num_abstractions; ++i) {
+            obj_constraint.insert(i, h_values_by_abstraction[i][abstract_state_ids[i]]);
         }
-    }
+        constraints.push_back(obj_constraint);
 
-    // Add constraint: sum_i w_i * h_i >= prev_obj_value
-    lp::LPConstraint obj_constraint(prev_obj_value, infinity);
-    for (int i = 0; i < num_abstractions; ++i) {
-        obj_constraint.insert(i, h_values_by_abstraction[i][abstract_state_ids[i]]);
-    }
-    constraints.push_back(obj_constraint);
+        // Add constraints: b_i * M >= w_i for each i
+        for (int i = 0; i < num_abstractions; ++i) {
+            lp::LPConstraint bin_constraint(0, infinity);
+            bin_constraint.insert(i, -1); // -w_i
+            bin_constraint.insert(num_abstractions + i, M); // +M * b_i
+            constraints.push_back(bin_constraint);
+        }
 
-    // Add constraints: b_i * M >= w_i for each i
-    for (int i = 0; i < num_abstractions; ++i) {
-        lp::LPConstraint bin_constraint(0, infinity);
-        bin_constraint.insert(i, -1); // -w_i
-        bin_constraint.insert(num_abstractions + i, M); // +M * b_i
-        constraints.push_back(bin_constraint);
-    }
+        // Set objective: minimize sum_i b_i * N_i
+        for (int i = 0; i < num_abstractions; ++i) {
+            int N_i = abstractions[i]->get_num_states(); // number of abstract states for abstraction i
+            variables[num_abstractions + i].objective_coefficient = N_i;
+        }
 
-    // Set objective: minimize sum_i b_i * N_i
-    for (int i = 0; i < num_abstractions; ++i) {
-        int N_i = abstractions[i]->get_num_states(); // number of abstract states for abstraction i
-        variables[num_abstractions + i].objective_coefficient = N_i;
-    }
+        // Build and solve the new LP
+        lp::LinearProgram new_lp(
+            lp::LPObjectiveSense::MINIMIZE, std::move(variables), std::move(constraints), infinity);
+        print_lp_solver.load_problem(new_lp);
+        print_lp_solver.solve();
 
-    // Build and solve the new LP
-    lp::LinearProgram new_lp(
-        lp::LPObjectiveSense::MINIMIZE, std::move(variables), std::move(constraints), infinity);
-    lp_solver.load_problem(new_lp);
-    lp_solver.solve();
-
-    vector<double> min_solution = lp_solver.extract_solution();
-    for (int i = 0; i < num_abstractions; ++i) {
-        double b_i = min_solution[num_abstractions + i];
-        if (b_i > 0.5) {
-            const Projection *proj = dynamic_cast<const Projection *>(abstractions[i].get());
-            if (proj) {
-                const vector<int> &pattern = proj->get_pattern();
-                cout << "Selected pattern for abstraction " << i << ": ";
-                for (int var : pattern) {
-                    cout << var << " ";
+        vector<double> min_solution = print_lp_solver.extract_solution();
+        for (int i = 0; i < num_abstractions; ++i) {
+            double b_i = min_solution[num_abstractions + i];
+            if (b_i > 0.5) {
+                const Projection *proj = dynamic_cast<const Projection *>(abstractions[i].get());
+                if (proj) {
+                    const vector<int> &pattern = proj->get_pattern();
+                    cout << "Selected pattern for abstraction " << i << ": ";
+                    for (int var : pattern) {
+                        cout << var << " ";
+                        cout << task_proxy.get_variables()[var].get_fact(0).get_name().erase(0, 5) << " ";
+                    }
+                    cout << endl;
                 }
-                cout << endl;
             }
         }
     }
-
     return cp_heuristic;
 }
 
@@ -234,10 +250,11 @@ public:
         document_synopsis(
             "Compute the maximum over multiple PhO heuristics precomputed offline.");
 
-        add_options_for_cost_partitioning_heuristic(*this, "pho");
-        add_option<bool>("saturated", "saturate costs", "true");
-        add_order_options(*this);
-        lp::add_lp_solver_option_to_feature(*this);
+    add_options_for_cost_partitioning_heuristic(*this, "pho");
+    add_option<bool>("saturated", "saturate costs", "true");
+    add_option<bool>("ppc", "enable post-processing constraint LP", "false");
+    add_order_options(*this);
+    lp::add_lp_solver_option_to_feature(*this);
     }
 
     virtual shared_ptr<ScaledCostPartitioningHeuristic> create_component(
@@ -254,7 +271,9 @@ public:
             abstractions, costs, options.get<lp::LPSolverType>("lpsolver"),
             options.get<bool>("saturated"),
             utils::get_log_for_verbosity(
-                options.get<utils::Verbosity>("verbosity")));
+                options.get<utils::Verbosity>("verbosity")),
+            scaled_costs_task,
+            options.get<bool>("ppc"));
         CPFunction cp_function = [&pho](
                                      const Abstractions &abstractions_,
                                      const vector<int> &order_,
--- src/search/cost_saturation/pho_heuristic.h
+++ src/search/cost_saturation/pho_heuristic.h
@@ -5,21 +5,27 @@
 
 #include "../lp/lp_solver.h"
 #include "../utils/logging.h"
+#include "utils.h"
 
 #include <vector>
 
 namespace cost_saturation {
 class PhO {
     lp::LPSolver lp_solver;
+    lp::LPSolverType solver_type; // store solver type for second LP
     std::vector<std::vector<int>> h_values_by_abstraction;
     bool saturated;
     std::vector<std::vector<int>> saturated_costs_by_abstraction;
     utils::LogProxy log;
+    std::shared_ptr<AbstractTask> task_ptr;
+    bool ppc;
 
 public:
     PhO(const Abstractions &abstractions, const std::vector<int> &costs,
         lp::LPSolverType solver_type, bool saturated,
-        const utils::LogProxy &log);
+        const utils::LogProxy &log,
+        std::shared_ptr<AbstractTask> task_ptr,
+        bool ppc);
 
     CostPartitioningHeuristic compute_cost_partitioning(
         const Abstractions &abstractions, const std::vector<int> &order,
